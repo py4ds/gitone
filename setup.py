691da7e7b68db19968a845b6d279a0a50065b605
Martin Skarzynski <marskar@gmail.com>
2019-05-12 10:29:30 -0400

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setuptools.setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/marskar/gitone',
-    version='0.0.11',
+    version='0.0.12',
     zip_safe=False,
 )
