b07f392ba4be9b686aa6b52a91cbf9e43fcc129d
Martin Skarzynski <marskar@gmail.com>
2019-05-17 00:33:18 -0400

    Bump version: 0.0.26 → 0.0.27

--- src/gitone/__init__.py
+++ src/gitone/__init__.py
@@ -10,7 +10,7 @@ from gitone.aamend import aamend
 from gitone.aamendp import aamendp
 
 __author__ = "Martin Skarzynski"
-__version__ = '0.0.26'
+__version__ = '0.0.27'
 __all__ = [
     "camp",
     "acmp",
